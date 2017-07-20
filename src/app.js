16b60b51b13b66eae63b56e1a54530973b5f2115
Maciej Kienig <maciejkienig@gmail.com>
2017-07-20 23:07:48 +0200

    adding hover elements on pictures

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@ import 'html-loader?interpolate!./index.html';
 
 console.info('qwe');
 import './js/hamburgerMenu.js';
+import './js/pictureHover.js';
 
 import './scss/app.scss';
---
+++ src/js/pictureHover.js
@@ -0,0 +1,31 @@
+// var picture = document.querySelectorAll('.work__items__image img');
+
+// for (i=0; i<picture.length; i++){
+//     console.log('działa?');
+//     console.log(picture[i]);
+//     picture[i].addEventListener('mouseover', function(e){
+//         console.log('czy na pewno?');
+//         this.nextElementSibling.style.opacity = '0';
+
+
+
+
+//     });
+// }
+
+
+
+// for(x in picture){
+//     console.log(picture[x]);
+//     var pic = picture[x];
+//     console.log(pic);
+//     pic.addEventListener('click', function(e){
+//         this.nextSibling.style.opacity = 1;
+//     })
+    // var cover = pic.nextSibling;
+    // this.addEventListener('mouseover', function(e){
+    //     console.log('o co makan?');
+    //     this.nextSibling.style.opacity = '1';
+    // })
+
+// }
